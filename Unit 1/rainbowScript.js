e5dc8b29ba6979e18a13f4da6aa75ebea0053ab0
Jono-hare <jono.hare.w@gmail.com>
2021-11-27 21:17:33 +0200

    Updated .js and alphabetsoup.html

--- Unit 1/rainbowScript.js
+++ Unit 1/rainbowScript.js
@@ -1,33 +1,40 @@
+// testing code
+let divColor = document.getElementsByClassName("box");
+Array.from(divColor).forEach((box) => {
+  console.log(box.id);
+});
+for (i = 0; i < divColor; i++) {
+  document.getElementById(divColor[i]).style.background = getElementById(
+    divColor[i]
+  );
+}
+
+// Working code
+
 function red() {
   document.getElementById("red").style.background = "red";
   document.getElementById("text-red").innerHTML = "Red";
 }
-
 function orange() {
   document.getElementById("orange").style.background = "orange";
   document.getElementById("text-orange").innerHTML = "Orange";
 }
-
 function yellow() {
   document.getElementById("yellow").style.background = "yellow";
   document.getElementById("text-yellow").innerHTML = "Yellow";
 }
-
 function green() {
   document.getElementById("green").style.background = "green";
   document.getElementById("text-green").innerHTML = "Green";
 }
-
 function blue() {
   document.getElementById("blue").style.background = "blue";
   document.getElementById("text-blue").innerHTML = "Blue";
 }
-
 function violet() {
   document.getElementById("violet").style.background = "#4B0082";
   document.getElementById("text-violet").innerHTML = "Violet";
 }
-
 function pink() {
   document.getElementById("pink").style.background = "#BD31FF";
   document.getElementById("text-pink").innerHTML = "Pink";
