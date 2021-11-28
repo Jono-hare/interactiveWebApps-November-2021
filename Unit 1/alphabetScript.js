910883deacda272424c2c241853e91fb3ad4b6e9
Jono-hare <jono.hare.w@gmail.com>
2021-11-28 20:12:12 +0200

    - completed alphabet soup - Missing last point of challenge

--- Unit 1/alphabetScript.js
+++ Unit 1/alphabetScript.js
@@ -1,12 +1,15 @@
-const text = document.getElementById("aPara").innerHTML;
+let text = document.getElementById("aPara").innerHTML;
 
 let alphabetSoup = function (text) {
   return [...text].sort().join("");
 };
+
 document.getElementById("btn").onclick = function () {
-  let soup = document.getElementById("aPara").value;
-  document.getElementById("emptyPara").innerHTML = "yes";
+  document.getElementById("emptyPara").innerHTML = alphabetSoup(text);
 };
 
-console.log(alphabetSoup(text));
 console.log(text);
+console.log(alphabetSoup(text));
+
+//  Could not complete
+// Get the value of the text input with an element's .value property
