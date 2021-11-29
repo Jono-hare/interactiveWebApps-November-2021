147c0b39774dc23c1c9f9b3caced23fe730978da
Jono-hare <jono.hare.w@gmail.com>
2021-11-29 11:37:49 +0200

    Completed character counter challenge

---
+++ characterScript.js
@@ -0,0 +1,11 @@
+let characterCounter;
+characterCounter = document.getElementById("form-textarea");
+characterCounter.addEventListener("keyup", countCharacters, false);
+
+function countCharacters(e) {
+  let textEntered, countRemaining, counter;
+  textEntered = document.getElementById("form-textarea").value;
+  counter = 300 - textEntered.length;
+  countRemaining = document.getElementById("count");
+  countRemaining.textContent = counter;
+}
