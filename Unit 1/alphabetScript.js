4c0b2d18221d87cdfd4545bcff92683d1744d8df
Jono-hare <jono.hare.w@gmail.com>
2021-11-29 12:27:54 +0200

    - updated alphabet challenge

--- Unit 1/alphabetScript.js
+++ Unit 1/alphabetScript.js
@@ -1,15 +1,37 @@
-let text = document.getElementById("aPara").innerHTML;
+// First  attempt
 
-let alphabetSoup = function (text) {
-  return [...text].sort().join("");
-};
+// let text;
+// text = document.getElementById("word").innerHTML;
 
-document.getElementById("btn").onclick = function () {
-  document.getElementById("emptyPara").innerHTML = alphabetSoup(text);
-};
+// let alphabetSoup = function (text) {
+//   return [...text].sort().join("");
+// };
 
-console.log(text);
-console.log(alphabetSoup(text));
+// document.getElementById("btn").onclick = function () {
+//   document.getElementById("newWord").innerHTML = alphabetSoup(text);
+// };
+
+// console.log(text);
+// console.log(alphabetSoup(text));
 
 //  Could not complete
 // Get the value of the text input with an element's .value property
+
+// second attempt
+
+let string, scrambleLetters, soupArray;
+
+string = document.getElementById("word").innerHTML;
+
+scrambleLetters = function (string) {
+  soupArray = Array.from(string).sort().join("");
+  return soupArray;
+};
+
+// soupArray.value = newWord;
+
+document.getElementById("btn").onclick = function () {
+  document.getElementById("word").innerHTML = scrambleLetters(string);
+};
+console.log(scrambleLetters(string));
+console.log(soupArray);
