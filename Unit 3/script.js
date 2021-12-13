e061719931b4d8d418126e42a92d646905c80547
Jono-hare <jono.hare.w@gmail.com>
2021-12-13 15:09:14 +0200

    updated challenge 1 from Unit 3

--- Unit 3/script.js
+++ Unit 3/script.js
@@ -4,7 +4,24 @@ const dueDate = document.querySelector("#dueDate");
 const dueTime = document.querySelector("#dueTime");
 const submitBtn = document.querySelector("#submitBtn");
 
-// testing code
-console.log(typeof dueDate);
-console.log(typeof dueTime);
-console.log(typeof newTask);
+// challenge 1 - Add custom instructions
+
+submitBtn.addEventListener("click", () => {
+  if (newTask.value === "") {
+    newTask.setCustomValidity("Required and must contain no HTML characters.");
+  } else {
+    newTask.setCustomValidity("");
+  }
+
+  if (dueDate.value == null || dueDate.value == "") {
+    dueDate.setCustomValidity("Required, must select a date.");
+  } else {
+    dueDate.setCustomValidity("");
+  }
+
+  if (dueTime.value == null || dueTime.value == "") {
+    dueTime.setCustomValidity("Required, must select a time.");
+  } else {
+    dueTime.setCustomValidity("");
+  }
+});
