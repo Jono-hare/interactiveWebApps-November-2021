1b5d1bdb8234db53aa741e8cc5cc9fed163d40bc
Jono-hare <jono.hare.w@gmail.com>
2021-12-13 13:28:34 +0200

    - updated all files in unit 3

---
+++ Unit 3/script.js
@@ -0,0 +1,10 @@
+const newTask = document.querySelector("#newTask");
+const taskDescription = document.querySelector("#taskDescription");
+const dueDate = document.querySelector("#dueDate");
+const dueTime = document.querySelector("#dueTime");
+const submitBtn = document.querySelector("#submitBtn");
+
+// testing code
+console.log(typeof dueDate);
+console.log(typeof dueTime);
+console.log(typeof newTask);
